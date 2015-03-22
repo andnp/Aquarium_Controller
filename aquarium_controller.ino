fc0fde030140a1a23a694978dcf6184f17242799
andnp <soundguyandyp@gmail.com>
2015-03-22 00:08:21 -0400

    Upgrade to Ultrasonic Sensor

--- aquarium_controller.ino
+++ aquarium_controller.ino
@@ -1,28 +1,37 @@
+//#include <HC_SR04.h>
+#include <NewPing.h>
+
+//HC_SR04 ultrasonic(12,13);
+NewPing ultrasonic(12,13,200);
+
+int i = 0;
 void setup()
 {
-  pinMode(7,OUTPUT);
-  digitalWrite(7,HIGH);
+  pinMode(11,OUTPUT);
+  digitalWrite(11,HIGH);
   pinMode(8,OUTPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  int inp = averageRead(100);
-  if(inp > 950){
-    digitalWrite(8, HIGH);
-    delay(15000);
-  } else {
+  //int inp = ultrasonic.smoothMedianFilter(500);
+  int inp = ultrasonic.convert_cm(ultrasonic.ping_median(50));
+  if(inp < 8 && i > 25){
     digitalWrite(8, LOW);
+    long start = millis();
+    while(inp < 10){
+      //inp = ultrasonic.smoothMedianFilter(500);
+      inp = ultrasonic.convert_cm(ultrasonic.ping_median(50));
+      Serial.println(inp);
+      delay(100);
+      if(millis() - start > (1000 * 30)) break;
+    }
+  } else {
+    digitalWrite(8, HIGH);
+    i++;
   }
-  delay(1000);
   Serial.println(inp);
+  delay(100);
 }
 
-int averageRead(int trials){
-  float out = 0;
-  for(int i = 0; i <= trials; i++){
-    out += analogRead(A0);
-  }
-  return out / trials;
-}
